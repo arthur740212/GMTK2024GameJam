4931187fe717eee0ba98bd9a15fb0b70a2c5834d
Mark Chen (I Hsuan Chen) <markchen0819@gmail.com>
2024-08-18 19:30:08 -0700

    UI update

---
+++ Assets/Scripts/Move3DObjectToUI.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move3DObjectToUI : MonoBehaviour
+{
+    public Transform objectToMove;
+    public RectTransform uiTarget;
+    public float nearPlaneOffset = 5f;
+    public float movementSpeed = 50.0f;
+
+    private void Start()
+    {
+        objectToMove = this.gameObject.transform;
+
+        GameObject uiGameObject = GameObject.Find("UITargetImage");
+        if (uiGameObject != null)
+        {
+            uiTarget = uiGameObject.GetComponent<RectTransform>();
+            if (uiTarget == null)
+            {
+                Debug.LogError("No RectTransform found on the GameObject named UITargetImage");
+            }
+        }
+    }
+
+    void Update()
+    { 
+        Vector3 uiTargetWorldPos = Camera.main.ScreenToWorldPoint(new Vector3(uiTarget.position.x, uiTarget.position.y, Camera.main.nearClipPlane + nearPlaneOffset));
+        objectToMove.position = Vector3.MoveTowards(objectToMove.position, uiTargetWorldPos, movementSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(objectToMove.position, uiTargetWorldPos) < 0.1f)
+        {
+            Destroy(objectToMove.gameObject);
+        }
+    }
+}
