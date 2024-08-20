7aac5ce5879c54e05dc8ba1c5703d06888383d3c
arthur272 <36757173+arthur740212@users.noreply.github.com>
2024-08-20 04:23:51 -0700

    fix(audio) : prevent duplicate audio

--- Assets/Scripts/AudioMaster.cs
+++ Assets/Scripts/AudioMaster.cs
@@ -2,9 +2,19 @@ using UnityEngine;
 
 public class AudioMaster : MonoBehaviour
 {
-
+    static GameObject permanentGO;
     void Awake()
     {
         DontDestroyOnLoad(gameObject);
+
+        if (permanentGO == null) 
+        {
+            permanentGO = this.gameObject;
+        }
+        if (permanentGO != this.gameObject)
+        {
+            Destroy(this.gameObject);
+        }
+       
     }
 }
